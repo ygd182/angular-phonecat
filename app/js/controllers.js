270ec78571cf75272195e580ebf439838918cbf4
Jorge Bolpe <jorge.bolpe@globant.com>
2015-06-11 10:36:52 -0300

    step 7 done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,18 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', ['phonecatServices']);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'PhoneServices',
+  function($scope, PhoneServices) {
+  	var onGetPhonesSuccess = function(response){
+  		$scope.phonesList = response.data;
+  	};
+
+  	var onGetPhonesFailure = function(){
+  		console.log('error');
+  	};
+
+  	PhoneServices.then(onGetPhonesSuccess, onGetPhonesFailure);
+  }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,9 @@
 
 /* Services */
 
+var phonecatServices = angular.module('phonecatServices', []);
+
+phonecatServices.service('PhoneServices', ['$http',
+  function($http) {
+  	return $http.get('phones/phones.json');
+  }]);
